e008f9731611f5c8e46f5c8c4e79b49dab58b806
Travis Milner <travismilner1@gmail.com>
2021-02-13 11:16:05 -0600

    added joined property to events

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -5,3 +5,11 @@ class Event(models.Model):
     game = models.ForeignKey("Games", on_delete=models.CASCADE)
     location = models.CharField(max_length=200)
     scheduler = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -96,12 +96,23 @@ class Events(ViewSet):
         Returns:
             Response -- JSON serialized list of events
         """
+        gamer = Gamer.objects.get(user=request.auth.user)
         events = Event.objects.all()
+
+        # Set the `joined` property on every event
+        for event in events:
+            event.joined = None
+
+            try:
+                EventGamers.objects.get(event=event, gamer=gamer)
+                event.joined = True
+            except EventGamers.DoesNotExist:
+                event.joined = False
 
         # Support filtering events by game
         game = self.request.query_params.get('gameId', None)
         if game is not None:
-            events = events.filter(game__id=game)
+            events = events.filter(game__id=type)
 
         serializer = EventSerializer(
             events, many=True, context={'request': request})
@@ -191,5 +202,5 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ('id', 'game', 'scheduler',
-                  'location', 'event_time')
+                  'location', 'event_time', 'joined')
 
